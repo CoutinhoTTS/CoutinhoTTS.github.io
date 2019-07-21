e278f6e5966d1addaee723af42b9e3f644b62112
CoutinhoTTS <48982306+CoutinhoTTS@users.noreply.github.com>
2019-07-21 22:56:33 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var audio = document.getElementById('audio')
+let audioPlay = audio.play();
+audioPlay.catch((err)=>{
+    console.log(err);
+    console.log("不允许自动播放");
+    console.log(audio)
+
+})
+$('#yes').on('click',function () {
+    var passworld = $('#passworld').val()
+    if (passworld == '0615') {
+        audio.play()
+        $('.condition').addClass('animated fadeOutLeftBig')
+        $('.condition').css('display','none')
+        $('.conditions').css('display','block')
+        var s = 3
+        $('#sin').text(s)
+        var iCount
+        iCount = setInterval(function () {
+            s = s-1
+            $('#sin').html(s)
+            if (s == -1) {
+                clearInterval(iCount)
+                $('.conditions>div:first-child').css('display','none')
+                $('.conditions>div:last-child').css('display','block')
+            }
+        },1000)
+    }})
